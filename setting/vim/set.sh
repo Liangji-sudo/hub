8065bc207b8b7ee417d89f00a399f69f2f380585
Liangji-sudo <15262272286@163.com>
2025-08-31 10:55:40 +0800

    feat: add vim into setting
    Signed-off-by: Liangji-sudo <15262272286@163.com>

---
+++ setting/vim/set.sh
@@ -0,0 +1,9 @@
+# this script will help you to install vim and set it quickly
+
+#install
+echo " ====================> liangji start install vim"
+sudo apt-get install vim
+
+#set vimrc
+echo " ====================> liangji start setting vim"
+sudo cp ./vimrc /etc/vim/vimrc
