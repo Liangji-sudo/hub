ccc6537dc0c37daf242a42d729aa58f1f27699b3
liangji_wsl <15262272286@163.com>
2025-09-26 21:43:41 +0800

    update: update vim set.sh
    Signed-off-by: liangji_wsl <15262272286@163.com>

--- setting/vim/set.sh
+++ setting/vim/set.sh
@@ -3,10 +3,12 @@
 #install
 echo " ====================> liangji start install vim"
 sudo apt-get install vim
+echo " ====================> liangji finish install"
 
 #set vimrc
 echo " ====================> liangji start setting vim"
 sudo cp ./vimrc /etc/vim/vimrc
+echo " ====================> liangji finish setting vim"
 
 #set ai 
 #
