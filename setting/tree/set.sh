a758fbae042a195700f29c2e0b3bafb842360158
liangji_wsl <15262272286@163.com>
2025-09-26 21:42:43 +0800

    feat: install tree
    Signed-off-by: liangji_wsl <15262272286@163.com>

---
+++ setting/tree/set.sh
@@ -0,0 +1,6 @@
+# this script will help you to install tree quickly
+
+#install
+echo " ====================> liangji start install tree"
+sudo apt-get install tree
+echo " ====================> liangji finish it"
