3aef14631843099d5be092d11e703356f4b365a5
liangji_wsl <15262272286@163.com>
2025-09-26 22:19:50 +0800

    feat: install ctags into vim
    Signed-off-by: liangji_wsl <15262272286@163.com>

--- setting/vim/set.sh
+++ setting/vim/set.sh
@@ -10,5 +10,21 @@ echo " ====================> liangji start setting vim"
 sudo cp ./vimrc /etc/vim/vimrc
 echo " ====================> liangji finish setting vim"
 
+
+
+# install some good plugin
+mkdir ~/.vim
+
+#add ctags into vim
+#usage:
+#ctags -R .
+#找到光标的变量的定义ctrl ] 
+#回到原来的位置 ctrl t/o
+echo " ====================> liangji start installing vim(ctags)"
+sudo apt-get install universal-ctags
+whatis ctags
+echo " ====================> liangji finish installing"
+
+
 #set ai 
 #
