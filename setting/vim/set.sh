639c423a95499b47150b6ac86dc6dc9bec1a37b3
liangji_wsl <15262272286@163.com>
2025-09-27 09:33:29 +0800

    feat: install vim-plug
    Signed-off-by: liangji_wsl <15262272286@163.com>

--- setting/vim/set.sh
+++ setting/vim/set.sh
@@ -1,22 +1,26 @@
 # this script will help you to install vim and set it quickly
 
 #install
-echo " ====================> liangji start install vim"
+echo " ====================> install vim"
 sudo apt-get install vim
-echo -e " ====================> liangji finish install\n\n\n"
+echo -e " ====================> finish\n\n\n"
 
 #set vimrc
-echo " ====================> liangji start setting vim"
+echo " ====================> set vim"
 sudo cp ./vimrc /etc/vim/vimrc
 source /etc/vim/vimrc
-echo -e " ====================> liangji finish setting vim \n\n\n"
+echo -e " ====================> finish \n\n\n"
 
 
 # install some good plugin
+echo " ====================> install vim_plug"
 mkdir ~/.vim
 rm -rf ~/.vim/*
 mkdir ~/.vim/plugged ~/.vim/plugin ~/.vim/syntax ~/.vim/colors ~/.vim/doc ~/.vim/autoload
 
+# install vim-plug
+cp vim_plug/plug.vim ~/.vim/autoload/ 
+echo -e " ====================> finish \n\n\n"
 
 
 #add ctags into vim
@@ -24,38 +28,38 @@ mkdir ~/.vim/plugged ~/.vim/plugin ~/.vim/syntax ~/.vim/colors ~/.vim/doc ~/.vim
 #ctags -R .
 #找到光标的变量的定义ctrl ] 
 #回到原来的位置 ctrl t/o
-echo " ====================> liangji start installing vim(ctags)"
+echo " ====================> install vim(ctags)"
 sudo apt-get install universal-ctags
 whatis ctags
-echo -e " ====================> liangji finish installing\n\n\n"
+echo -e " ====================> finish\n\n\n"
 
 
 #add NERDTree into vim
 #usage:
 #:NERDTree
-echo " ====================> liangji start installing vim(NERDTree)"
+echo " ====================> install vim(NERDTree)"
 #unzip ./nerdtree.zip -d ~/.vim
 cp ./nerdtree/doc/* ~/.vim/doc/
 cp -rf ./nerdtree/nerdtree_plugin ~/.vim/
 cp ./nerdtree/plugin/* ~/.vim/plugin
 cp ./nerdtree/syntax/* ~/.vim/syntax
 #if wanna start NERDTree auto, see vimrc directly
-echo -e " ====================> liangji finish installing\n\n\n"
+echo -e " ====================> finish\n\n\n"
 
 #add Taglist into vim
 #usage:
 #:Tlist
-echo " ====================> liangji start installing vim(Tlist)"
+echo " ====================> install vim(Tlist)"
 cp ./taglist/taglist.txt ~/.vim/doc
 cp ./taglist/taglist.vim ~/.vim/plugin
-echo -e " ====================> liangji finish installing\n\n\n"
+echo -e " ====================> finish\n\n\n"
 
 
 #add winmanager
-echo " ====================> liangji start installing vim(winmanager)"
+echo " ====================> install vim(winmanager)"
 cp ./winmanager/doc/* ~/.vim/doc
 cp ./winmanager/plugin/* ~/.vim/plugin
-echo -e " ====================> liangji finish installing\n\n\n"
+echo -e " ====================> finish\n\n\n"
 
 
 
