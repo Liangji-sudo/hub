486891d599c28a2612926505246bab6fc4398703
liangji <15262272286@163.com>
2025-10-02 09:42:58 +0800

    feat: install tig
    Signed-off-by: liangji <15262272286@163.com>

--- setting/git/set.sh
+++ setting/git/set.sh
@@ -3,4 +3,12 @@
 #update
 echo " ====================> liangji start update git use vim as default editor"
 git config --global core.editor vim
-echo " ====================> liangji update git over"
+echo " ====================> over\n\n\n"
+
+echo " ====================> start installing tig"
+sudo apt install tig
+echo " ====================> over\n\n\n"
+
+
+
+
